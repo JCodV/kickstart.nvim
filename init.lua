517614f7521fb54b73e7e05189a8a2028ced67f4
JCodV <jasonvasqf@gmail.com>
2025-09-26 17:23:41 -0400

    changed preset to none for cmp as tab was still using the snippet shortcut sometimes

--- init.lua
+++ init.lua
@@ -817,7 +817,8 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        -- preset = 'default',
+        preset = 'none',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
